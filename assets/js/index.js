29c606115fa5aaf6e1820e10a7f8549033395b60
Tkachenko Anna <hanna.tkachenko13@gmail.com>
2022-06-23 22:56:25 +0300

    Add functions fullName & createCard

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,26 @@
-
 "use strict";
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 const mapSocialClass = new Map();
-mapSocialClass.set('www.facebook.com', "fa-facebook");
-mapSocialClass.set('twitter.com', "fa-twitter");
-mapSocialClass.set('www.instagram.com', "fa-instagram");
-
+mapSocialClass.set("www.facebook.com", "fa-facebook");
+mapSocialClass.set("twitter.com", "fa-twitter");
+mapSocialClass.set("www.instagram.com", "fa-instagram");
 
 /**
- * 
- * @param {string} tag 
- * @param {object} options 
+ *
+ * @param {string} tag
+ * @param {object} options
  * @param {string[]} options.classNames
- * @param {objects} children 
- * @returns 
+ * @param {objects} children
+ * @returns
  */
- function createElement(tag='div', {classNames=[],attributes={},events={}}, ...children){
+createElement = (
+  tag = "div",
+  { classNames = [], attributes = {}, events = {} },
+  ...children
+) => {
   const element = document.createElement(tag);
-  if(classNames.length){
+  if (classNames.length) {
     element.classList.add(...classNames);
   }
   for (const [nameAttr, valueAttr] of Object.entries(attributes)) {
@@ -29,23 +31,34 @@ mapSocialClass.set('www.instagram.com', "fa-instagram");
   }
   element.append(...children);
   return element;
+};
+
+fullName = (user) => `${user.firstName}${user.lastName}`;
+
+ createCard = (user)=>{
+  const url = new URL(user.contacts[0]);
+  const a = document.createElement('a');
+  a.classList.add(mapSocialClass.get(url.hostname));
+  
+  return createElement('article',{}, document.createTextNode(user.fullName()) );
 }
+
 
 
 
 fetch("./data.json")
   .then((response) => response.json())
-  .then((users) => {   
-    const cards = users.map((user)=>createCard(user));
+  .then((users) => {
+    const cards = users.map((user) => createCard(user));
     root.append(...cards);
   })
   .catch((error) => {
-    document.body.prepend(document.createTextNode('500'));
+    document.body.prepend(document.createTextNode("500"));
     if (error instanceof TypeError) {
       console.error("Ошибка соединения: ", error);
-    }else if (error instanceof SyntaxError) {
+    } else if (error instanceof SyntaxError) {
       console.error("Проверь запятые: ", error);
-    }else{
+    } else {
       console.error(error);
     }
-  })
+  });
