ba51a33f7ef0b75abef9e5663a1d60ac56b3896b
Tkachenko Anna <hanna.tkachenko13@gmail.com>
2022-06-23 23:02:55 +0300

    Add stringToColour

--- assets/js/index.js
+++ assets/js/index.js
@@ -43,7 +43,18 @@ fullName = (user) => `${user.firstName}${user.lastName}`;
   return createElement('article',{}, document.createTextNode(user.fullName()) );
 }
 
-
+ stringToColour= (str) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = "#";
+  for (let i = 0; i < 3; i++) {
+    let value = (hash >> (i * 8)) & 0xff;
+    colour += ("00" + value.toString(16)).slice(-2);
+  }
+  return colour;
+}
 
 
 fetch("./data.json")
